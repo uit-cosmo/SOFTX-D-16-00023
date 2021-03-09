96a3d4520f9c94a88237a4089201826f83cb1ba0
Nils <nils.bochow@uit.no>
2021-03-09 12:04:34 +0100

    Script to generate albedo for the 1st year.
    This script generates a .dat file of the calculated albedo from the temperature values of the unchanged model and the generated geography.
    This is mainly to match the albedo distribution of the time independent albedo calculation with the time dependent albedo calculation.
    Respects the different threshold temperatures for ice forming over water/land.

--- EBM/preprocess/albedo_year_1.py
+++ EBM/preprocess/albedo_year_1.py
@@ -3,7 +3,8 @@ import netCDF4
 import datetime
 from matplotlib import pyplot as plt
 from netCDF4 import Dataset,num2date,date2num
-dataset = '../FortranCode/EBM/output/timesteps-output2.nc'
+import matplotlib.animation as animation
+dataset = '../FortranCode/EBM/output/timesteps-output_original.nc'
 geography = np.genfromtxt('EBM/preprocess/geography_0.dat', dtype=int, delimiter=1)
 
 ny = 65
@@ -18,9 +19,10 @@ def define_albedo_1st_year():
     albedo = np.zeros((48, ny,nx))
     temp_mask = np.zeros((ny,nx))
     for t in range(48): 
-        temp_mask[temp[t, :, :][0, :, :] <= -1] = 1
-        temp_mask[temp[t, :, :][0, :, :] > -1] = 0
-        
+        temp_mask[(temp[0*48 + t, :, :][0, :, :] <= -1) & (geography == 1)] = 1 # sea ice
+        temp_mask[(temp[0*48 + t, :, :][0, :, :] > -1) & (geography == 1)] = 0 # sea without ice
+        temp_mask[(temp[0*48 + t, :, :][0, :, :] <= -5) & (geography == 2)] = 1 # land ice
+        temp_mask[(temp[0*48 + t, :, :][0, :, :] > -5) & (geography == 2)] = 0 # land without ice
 
         # land without ice 
         albedo[t, (geography == 1) & (temp_mask == 0)] = 0.3 + 0.09 * legendre(np.where((geography == 1) & (temp_mask == 0))[0])
@@ -30,7 +32,23 @@ def define_albedo_1st_year():
         albedo[t, (geography == 1) & (temp_mask == 1)] = 0.7
         # ocean without ice 
         albedo[t, (geography == 2) & (temp_mask == 0)] = 0.29 + 0.09 * legendre(np.where((geography == 2) & (temp_mask == 0))[0])
-    #np.savetxt('EBM/preprocess/albedo_year_1.dat', albedo, fmt= "%i",delimiter = '')
+    return albedo
+
+def calculate_albedo(t, temperature): 
+    albedo = np.zeros((ny, nx))
+    temp_mask = np.zeros((ny,nx))
+    temp_mask[temp[t, :, :][0, :, :] <= -1] = 1
+    temp_mask[temp[t, :, :][0, :, :] > -1] = 0
+        
+
+    # land without ice 
+    albedo[(geography == 1) & (temp_mask == 0)] = 0.3 + 0.09 * legendre(np.where((geography == 1) & (temp_mask == 0))[0])
+    # sea ice
+    albedo[(geography == 2) & (temp_mask == 1)] = 0.6
+    # land with ice 
+    albedo[(geography == 1) & (temp_mask == 1)] = 0.7
+    # ocean without ice 
+    albedo[(geography == 2) & (temp_mask == 0)] = 0.29 + 0.09 * legendre(np.where((geography == 2) & (temp_mask == 0))[0])
     return albedo
 albedo = define_albedo_1st_year()
 
@@ -39,24 +57,26 @@ def save_netCDF():
     
     nsteps = 48;
     unout = 'UNLIMITED'
-    # -----------------------
 
     datesout = [datetime.datetime(1+iyear,1,1) for iyear in range(nsteps)]; # create datevalues
-    # =========================
-    ncout = Dataset('albedo_year_1.nc','w','NETCDF4'); # using netCDF3 for output format 
+
+    ncout = Dataset('albedo_year_1.nc','w','NETCDF4'); 
+    ncout.createDimension('level',1)
     ncout.createDimension('latitude',ny)
     ncout.createDimension('longitude',nx)
     ncout.createDimension('time', None);
+    
     latvar = ncout.createVariable('latitude','float32',('latitude'));
-    latvar = lat
-    lonvar = ncout.createVariable('longitude','float32',('longitude'));
-    print(lonvar[:])
-    lonvar = lon
-    print(lonvar[:])
+    latvar.setncattr('units','degrees_north')
+    latvar[:] = np.array(lat)
 
-    timevar = ncout.createVariable('time','int',('time'))#;timevar.setncattr('units',unout);
-    #timevar[:]=date2num(datesout,unout);
-    myvar = ncout.createVariable('albedo','float32',('time','latitude','longitude'));myvar.setncattr('units','dimensionless');myvar[:] = albedo;
+    lonvar = ncout.createVariable('longitude','float32',('longitude'));
+    lonvar.setncattr('units','degrees_east')
+    lonvar[:] = np.array(lon)
+    
+    myvar = ncout.createVariable('ALBEDO','float32',('time', 'level', 'latitude', 'longitude'));myvar.setncattr('units','dimensionless');
+    myvar[:] = albedo;
+    
     ncout.close();
     
     
